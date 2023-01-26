055d6feed83bba4ba17ba21b67e84f536349a68e
Danesh Tolani <danesh.tolani@gmail.com>
2023-01-26 11:06:03 +0530

    stylized search page

--- src/views/SearchPage.jsx
+++ src/views/SearchPage.jsx
@@ -1,4 +1,6 @@
 import { Box, createTheme } from "@mui/system";
+import { Button } from "@mui/material";
+
 import React, { useMemo, useState } from "react";
 import FlexBetween from "../components/FlexBetween";
 import LoadingScreen from "../components/LoadingScreen";
@@ -17,8 +19,8 @@ const SearchPage = () => {
   const flexStyle = {
     flexWrap: "wrap",
     columnGap: "50px",
-    justifyContent: "flex-start",
-    alignItems: "start",
+    justifyContent: "center",
+    alignItems: "center",
     width: "100%",
   };
 
@@ -54,32 +56,41 @@ const SearchPage = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-y-5">
-          <input
-            className=" bg-red-600"
-            value={values.company}
-            onChange={handleInputChange}
-            name="movie"
-            label="Movie"
-          />
-          <input
-            className=" bg-red-600"
-            value={values.position}
-            onChange={handleInputChange}
-            name="year"
-            label="Year"
-          />
-          <button type="submit"> Submit </button>
-        </div>
-      </form>
+      <FlexBetween style={{ justifyContent: "center", padding: "1rem" }}>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-y-5 items-center">
+            <input
+              className=" "
+              value={values.company}
+              onChange={handleInputChange}
+              name="movie"
+              label="Movie"
+              style={{ border: `1px solid ${borderColor}`, backgroundColor: backgroundColor, color: textColor, padding: "0 1rem", width: "20rem", fontSize: "20px", borderRadius: "5px" }}
+            />
+            <input
+              className=" "
+              value={values.position}
+              onChange={handleInputChange}
+              name="year"
+              label="Year"
+              style={{ border: `1px solid ${borderColor}`, backgroundColor: backgroundColor, color: textColor, padding: "0 1rem", width: "20rem", fontSize: "20px", borderRadius: "5px" }}
+            />
+            <Button
+              variant="outlined"
+              type="submit"
+              sx={{ fontWeight: "600", border: "2px solid", width: "5rem" }}>
+              Submit
+            </Button>
+          </div>
+        </form>
+      </FlexBetween>
       {loading ? (
-        <FlexBetween style={{ justifyContent: "center" }}>
+        <FlexBetween style={{ justifyContent: "center", height: "67vh" }}>
           <LoadingScreen />
         </FlexBetween>
       ) : (
         <Box
-          style={{ backgroundColor: backgroundColor, height: "65vh", overflowY: "scroll" }}
+          style={{ backgroundColor: backgroundColor, height: "67vh", overflowY: "scroll" }}
           className="no-scrollbar ">
           <h1
             className="text-center text-3xl pt-4"
